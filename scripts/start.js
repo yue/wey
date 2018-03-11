b182536f35eba9bfe74cb3f15d8384a57af6eb5d
Cheng Zhao <zcbenz@gmail.com>
2018-03-11 12:48:52 +0900

    Add npm start

---
+++ scripts/start.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const path = require('path')
+const {spawn} = require('child_process')
+
+const root = path.resolve(__dirname, '..')
+const version = 'v' + require('../package.json').engines.yode
+const yode = path.resolve(root, 'yode',
+                          `yode-${version}-${process.platform}-${process.arch}`,
+                          process.platform == 'win32' ? 'yode.exe' : 'yode')
+
+const child = spawn(yode, [root])
+child.stdout.pipe(process.stdout)
+child.stderr.pipe(process.stderr)
