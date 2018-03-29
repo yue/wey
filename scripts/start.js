87e7f7436e1f709ecd37079f3e5c68cb92f53b67
Cheng Zhao <zcbenz@gmail.com>
2018-03-29 16:38:27 +0900

    Enable creating new main windows

--- scripts/start.js
+++ scripts/start.js
@@ -9,6 +9,6 @@ const yode = path.resolve(root, 'yode',
                           `yode-${version}-${process.platform}-${process.arch}`,
                           process.platform == 'win32' ? 'yode.exe' : 'yode')
 
-const child = spawn(yode, [root])
+const child = spawn(yode, ['--expose-gc', root])
 child.stdout.pipe(process.stdout)
 child.stderr.pipe(process.stderr)
