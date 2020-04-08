1e73679fb4cca6cd18cb184ab0f1da9e2f16d057
Cheng Zhao <zcbenz@gmail.com>
2020-04-08 21:32:50 +0900

    Add support for skin tone in emoji

--- scripts/generate_emoji_json.js
+++ scripts/generate_emoji_json.js
@@ -4,8 +4,10 @@ const path = require('path')
 const fs = require('fs-extra')
 const axios = require('axios')
 
-const emojiJsonUrl = 'https://github.com/iamcal/emoji-data/raw/master/emoji.json'
+const emojiJsonUrl = 'https://github.com/iamcal/emoji-data/raw/v4.1.0/emoji.json'
 const target = path.resolve(__dirname, '..', 'lib', 'service', 'slack', 'emoji.json')
+
+main()
 
 async function main() {
   const {data} = await axios(emojiJsonUrl)
@@ -13,11 +15,20 @@ async function main() {
   for (const d of data) {
     const codes = d.unified.split('-').map((u) => parseInt(u, 16))
     const native = String.fromCodePoint.apply(null, codes)
-    for (const name of d.short_names) {
-      outputJson[name] = {
-        x: d.sheet_x,
-        y: d.sheet_y,
-        native,
+    outputJson[d.short_name] = {
+      x: d.sheet_x,
+      y: d.sheet_y,
+    }
+    if (d.skin_variations) {
+      for (const k in d.skin_variations) {
+        const tone = skinToneToId(k)
+        if (tone < 0)
+          continue
+        const s = d.skin_variations[k]
+        outputJson[d.short_name + '::skin-tone-' + tone] = {
+          x: s.sheet_x,
+          y: s.sheet_y,
+        }
       }
     }
   }
@@ -25,4 +36,18 @@ async function main() {
   await fs.writeJson(target, outputJson)
 }
 
-main()
+function skinToneToId(skin) {
+  switch (skin) {
+    case '1F3FB': return 2
+    case '1F3FC': return 3
+    case '1F3FD': return 4
+    case '1F3FE': return 5
+    case '1F3FF': return 6
+    default: return -1
+  }
+}
+
+function codeToNative(unified) {
+  const codes = unified.split('-').map((u) => parseInt(u, 16))
+  return String.fromCodePoint.apply(null, codes)
+}
